83f889e6754a7f7b99f39edb999b784388f6df06
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-08-30 20:37:16 +0300

    Add the imports in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,11 @@
+import { Routes, Route } from "react-router-dom"
+import { Navbar } from "@/components/layout/Navbar"
+import { Footer } from "@/components/layout/Footer"
+import { Home } from "@/pages/Home"
+import { Explore } from "@/pages/Explore"
+import { Discovery } from "@/pages/Discovery"
+import { NearEarth } from "@/pages/NearEarth"
+
 function App() {
   return (
     <div></div>
