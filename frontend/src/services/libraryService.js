1e3a823dc2051985d89e665ec5650167b8490c46
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-08-30 20:26:07 +0300

    Add the function for fetching the media in libraryService

--- frontend/src/services/libraryService.js
+++ frontend/src/services/libraryService.js
@@ -7,3 +7,22 @@ const BASE_URL = "https://images-api.nasa.gov/search"
  * @param {string} query
  * @param {"image"|"video"|"audio"} [mediaType]
  */
+
+export async function searchLibrary(query, mediaType) {
+  const params = new URLSearchParams({ q: query })
+  if (mediaType) params.set("media_type", mediaType)
+
+  const data = await apiRequest(`${BASE_URL}?${params.toString()}`)
+  return (data.collection?.items || []).map((item) => {
+    const info = item.data?.[0] || {}
+    return {
+      id: info.nasa_id,
+      title: info.title,
+      description: info.description,
+      dateCreated: info.date_created,
+      mediaType: info.media_type,
+      center: info.center,
+      thumbnail: item.links?.[0]?.href,
+    }
+  })
+}
